44edd00574c72b1475c607ae35e5100a9d6a494d
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-01-25 17:26:46 +0200

    sh now throws

--- notifier/linux.js
+++ notifier/linux.js
@@ -2,10 +2,16 @@
 const sh = require('./sh');
 
 const cmd = 'notify-send';
-const which = sh`which ${cmd}`;
 const notify = opts => {
   const icon = opts.icon || 'terminal';
   sh.async`${cmd} -i ${icon} ${opts.title} ${opts.message}`;
 };
 
-module.exports = which.error ? () => {} : notify;
+module.exports = (() => {
+  try {
+    sh`which ${cmd}`;
+    return notify;
+  } catch (error) {
+    return () => {};
+  }
+})();
