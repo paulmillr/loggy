96d5302c82b302ac1eb22f69bdf6887e2fc19632
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-01-25 10:24:30 +0200

    fix notifications w/o on ubuntu

--- notifier/linux.js
+++ notifier/linux.js
@@ -3,9 +3,9 @@ const sh = require('./sh');
 
 const cmd = 'notify-send';
 const which = sh`which ${cmd}`;
-
 const notify = opts => {
-  sh.async`${cmd} -i ${opts.icon} ${opts.title} ${opts.message}`;
+  const icon = opts.icon || 'terminal';
+  sh.async`${cmd} -i ${icon} ${opts.title} ${opts.message}`;
 };
 
 module.exports = which.error ? () => {} : notify;
